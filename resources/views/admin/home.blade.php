0767f3e3ec98a9e1e20668bc10c4a1038ca833c1
arumuga007 <arumugaperumal0007@gmail.com>
2023-08-15 09:21:13 +0530

    initial commit

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,13 @@
+<x-app-layout>
+</x-app-layout>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Page</title>
+</head>
+<body>
+    <h1>Nothing</h1>
+</body>
+</html>
