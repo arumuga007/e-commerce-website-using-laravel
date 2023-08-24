ff186d58071558c95e13749273656f146050bd28
arumuga007 <arumugaperumal0007@gmail.com>
2023-08-24 21:36:37 +0530

    product Details page UI updated

--- resources/views/home/productDetails.blade.php
+++ resources/views/home/productDetails.blade.php
@@ -12,7 +12,7 @@
       <meta name="description" content="" />
       <meta name="author" content="" />
       <link rel="shortcut icon" href="images/favicon.png" type="">
-      <title>Famms - Fashion HTML Template</title>
+      <title>Whizoid Studio</title>
       <!-- bootstrap core css -->
       <link rel="stylesheet" type="text/css" href="home/css/bootstrap.css" />
       <link href="home/css/stylehome.css" rel="stylesheet" />
@@ -22,218 +22,67 @@
       <link href="home/css/style.css" rel="stylesheet" />
       <!-- responsive style -->
       <link href="home/css/responsive.css" rel="stylesheet" />
+      <link href="home/css/productStyle.css" rel="stylesheet" />
    </head>
-    <style>
-        .product-details {
-            display:flex;
-            flex-direction:row;
-            gap: 2vw;
-            width:100%;
-        }
-        .product-details-left {
-         display: flex;
-         flex-direction: column;
-         align-items: center;
-         
-         width: 40%;
-        }
-        .product-details-right {
-         width: 30%;
-         position: relative;
-        }
-        .details-img-container{
-            overflow: hidden;
-            height: 80%;
-            width: 100%;
-        }
-        img {
-            height: 100%;
-        }
-        .details-btn-container{
-            margin-top: 30px;
-            width: 100%;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            gap: 2vw;
-        }
-        .details-btn {
-            padding: 10px 50px;
-            color: white;
-            border-radius: 4px;
-            font-size: 1.2em;
-            font-weight: 700;
-        }
-      .product-title {
-         font-size: 2em;
-         font-weight: 800;
-      }
-
-      .given-price {
-         text-decoration: line-through;
-      }
-      .product-rate {
-         margin-left: 10px;
-         padding: 5px 9px;
-         background-color: green;
-         color: white;
-         border-radius: 2px;
-      }
-      .actual-price {
-         margin-top: 7px;
-         font-weight: 500;
-         font-size: 1.7em;
-         margin-right: 10px;
-      }
-
-      .discount-price {
-         color: green;
-         font-weight: 500;
-         margin-left: 15px;
-      }
-      .description-head {
-         margin: 10px 0;
-         font-size: 1.5em;
-      }
-      .category-subcategory {
-         margin-top: 10px;
-         font-weight: 500;
-         font-size: 1.5em;
-
-      }
-      .delivery-details {
-         font-size: 1.2em;
-         font-weight: 500;
-      }
-      .available-quantity {
-         margin-top: 10px;
-         font-weight: 500;
-         margin-top: 10px;
-      }
-
-      .product-quantity {
-         font-size: 1.2em;
-         font-weight: 500;
-         margin-top: 10px;
-      }
-      .quantity-input {
-         width: 150px;
-         border-radius: 5px;
-      }
-      .buy-btn {
-         padding: 15px 65px;
-      }
-      .prevent-execute {
-         
-         color:black;
-      }
-      .addcart-successful {
-         position: absolute;
-         background-color: black;
-         color: #1ABE4D;
-         text-align: center;
-         padding: 10px 20px;
-         border-radius: 5px;
-         bottom: 0;
-         opacity: 0;
-
-      }
-      .addcart-completed {
-         animation-name: show-success;
-         animation-duration: 5s;
-      }
-      @keyframes show-success {
-         0% {
-            opacity: 1;
-         }
-         10% {
-            bottom: 60px;
-         }
-         75% {
-            bottom: 60px;
-            opacity: 1;
-         }
-         90% {
-            opacity: 0;
-         }
-      }
-    </style>
    <body>
-      <div class="hero_area">
-        @include('home.header')
-        <div class="product-details">
-            <div class="product-details-left">
-                <div class="details-img-container">
-                  <img src="uploads/{{$data->image}}">
-                </div>
-                
+      
+<div class="addcart-successful" id='showsuccess'>
+   <i class="fa-solid fa-circle-check"></i> Product added to Cart successfully
+</div>
+<div class="container-scroller">
+      @include('home.header')
+      <div class="product-container">
+         <img src="uploads/{{$data->image}}" class="image-container">
+         <div class="product-details">
+            <div class="details-header">
+            {{$data->title}}
             </div>
-            <div class="product-details-right">
-               <div>
-               <span class="product-title">{{$data->title}}
-                  
-               </span>
-               <span class='product-rate'>4.2<i class="fa fa-star" aria-hidden="true" style="color:white; font-size: 10px; margin-left: 5px;"></i></span>
+            <div class="details-rating">
+            <i class="fa fa-star" aria-hidden="true" style="color: red; font-size: .8em;"></i>
+            <i class="fa fa-star" aria-hidden="true" style="color: red; font-size: .8em;"></i>
+            <i class="fa fa-star" aria-hidden="true" style="color: red; font-size: .8em;"></i>
+            <i class="fa fa-star" aria-hidden="true" style="color: red; font-size: .8em;"></i>
+            <i class="far fa-star" style="color: red; font-size: .8em;"></i>
+             <span  style="font-size: .8em;">(20)</span>
             </div>
-               <div class="product-price">
-                  <span class="actual-price">₹{{$data->price - $data->discount_price}}</span>
-                  <span class="given-price">₹{{$data->price}}</span>
-                  <span class="discount-price">{{floor($data->discount_price * 100 / $data->price)}}% off</span>
-
-               </div>
-                  <div class="product-description-container">
-                     <div class="description-head">Description</div>
-                     <div class="product-description">
-                            {{$data->description}}
-                     </div>
-                  </div>
-                  <div class="category-subcategory">
-                     <span style="color: #FF9F00">{{$data->category}}</span>   ||   <span style="color: #FB641B;">{{$data->subcategory}}</span>
-                  </div>
-                  <div class="addcart-successful" id='showsuccess'>
-                  <i class="fa-solid fa-circle-check"></i> product added to cart successfully
-                  </div>
-            </div>
-            <div class="product-details-rightmost">
-               <div class="delivery-details">Free Delivery <i class="fas fa-shipping-fast"></i></div>
-               <div class="product-stock">
-                  @if($data->quantity > 0)
-                     <div class="in-stock" style="color: green">In stock <i class="fa-solid fa-tag"></i></div>
-                  @else
-                     <div style="color: red;">Out of Stock</div>
-                  @endif
-               </div>
-               <div class="available-quantity">Available Quantity: <span>{{$data->quantity}}</span></div>
-               <div class="product-quantity">
-                  <form method="post">
+            <div class="description-header">Details:</div>
+            <div class="product-description">{{$data->description}}</div>
+            <div class="product-price"><span style="font-weight: 600; font-size: 1.4em; padding-right: 10px;">₹{{$data->price - $data->discount_price}}</span> <span style="text-decoration: line-through;font-size: 0.9em;">₹{{$data->price}}</span></div>
+            
+               <form method="post" class="quantity-container">
                      @csrf
                   <span>Quantity: </span>
-                  <input type=number value="1" min="1" onchange="checkQuantity()" class="quantity-input" id="quantity">
-                  </form>
-               </div>
-               <div class="details-btn-container">
-                  <button class="details-btn" style="background-color: #FF9F00;" onclick="addCarts(event)" id="addcart">
+                  <input type=number value="1" min="1" onchange="checkQuantity()" id="quantity">
+               </form>
+               <div class="product-btns">
+               <button class="product-btn" style="background-color: white;color: red;" onclick="addCarts(event)" id="addcart">
                      <i class="fa-solid fa-cart-shopping"></i>
                      Add to Cart 
                   </button>
-                  <button class="details-btn buy-btn" style="background-color: #FB641B;" onclick="buyNow(event)" id="buynow">
+                  <button class="product-btn buy-btn" style="padding: 10px 45px;" onclick="buyNow(event)" id="buynow">
                   <i class="fas fa-bolt"></i>
                      Buy Now 
-   </button>
-                </div>
+                  </button>
+               </div>
             </div>
-        </div>
-        @include('home.subscribe')
-        @include('home.client')
-        @include('home.footer') 
-      <div class="cpy_">
-         <p class="mx-auto">© 2021 All Rights Reserved By <a href="https://html.design/">Free Html Templates</a><br>
-         
-            Distributed By <a href="https://themewagon.com/" target="_blank">ThemeWagon</a>
-         
-         </p>
-      </div>   
+         </div>
+
+
+
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
       <!-- jQery -->
       <script src="home/js/jquery-3.4.1.min.js"></script>
       <!-- popper js -->
